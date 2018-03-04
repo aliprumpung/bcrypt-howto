1da7a1f7704b3e216fa9768955a63bfd9c1add8b
aliprumpung <callmesike@gmail.com>
2018-03-05 04:03:05 +0700

    create function compareRawPwdToHas using Promise

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 require('dotenv').config();
 var bcrypt = require('bcrypt');
-var bcrypt = require('bcrypt');
 const saltRounds = 10;
 var password = process.env.PASSWORD;
 
-bcrypt.hash(password,saltRounds,(error,hash)=>{
-console.log(hash);
-});
+const hashingPwd = (password,saltRounds)=>{
+	return new Promise((resolve,reject)=>{
+		bcrypt.hash(password,saltRounds,(error,hash)=>{
+			resolve(hash);
+		});
+	});
+};
+		
+
+const compareRawPwdToHash = (pwd,hashToCompare)=>{
+	return new Promise((resolve,reject)=>{
+		bcrypt.compare(pwd,hashToCompare, (err,resp)=>{
+			if(resp === true){
+				resolve(resp);
+			}else{
+				reject(new Error(resp));
+			}
+		});
+	});
+};
+
+
+hashingPwd(password,saltRounds)
+.then(hashResponse=>{
+	return compareRawPwdToHash('1234Aa$',hashResponse);
+})
+.then(isAuthenticated=>{
+	console.log(`isAuthenticated = ${isAuthenticated}`);
+})
+.catch(err=>{ console.log(`isAuthenticated = ${isAuthenticated}`) });
+
+
+/*
+run in console 
+
+output will be true or false 
+
+*/
+
+
+
+
+
+
+
+
 
